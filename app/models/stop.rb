6473c878e3de3e993ac4d1fae39260929e1dbd25
Robert Daly <rwilliamdaly@Roberts-MacBook-Pro.local>
2016-12-24 14:47:11 -0500

    stop model logic, referenced env variables differently

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,2 +1,9 @@
 class Stop < ApplicationRecord
+  has_many :route_stops, :dependent => :destroy
+  has_many :routes, :through => :route_stops
+  has_many :trips, :through => :routes
+
+  def trip_ids
+    trips.pluck(:id)
+  end
 end
--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -2,12 +2,12 @@ class TwilioService
   attr_reader :client
 
   def initialize
-    @client = Twilio::REST::Client.new ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN']
+    @client = Twilio::REST::Client.new Rails.application.secrets.account_sid, Rails.application.secrets.auth_token
   end
 
   def send_message(to, message)
     client.messages.create(
-      from: ENV['TWILIO_NUMBER'],
+      from: Rails.application.secrets.twilio_number,
       to: to,
       body: message
     )
