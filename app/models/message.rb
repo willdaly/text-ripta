0765f40b92d0270fe5b45e9b39ef0f4616675829
Robert Daly <rwilliamdaly@Roberts-MacBook-Pro.local>
2017-02-02 13:33:35 -0500

    moved vehicle sorting logic from messages into vps

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,33 +1,36 @@
 class Message < ApplicationRecord
 
   def text
-    stop ? success : error
+    stop ? success : "couldn't find a stop by the ID " + self.posted_text
   end
 
   private
 
   def stop
-    @stop ||= Stop.find_by_id(self.posted_text)
-  end
-
-  def error
-    self.posted_text + " is not a valid stop id"
+    @stop = Stop.find_by_id(self.posted_text)
   end
 
   def success
-    vehicles = VehiclePositionsService.new(stop.trip_ids).new_vehicles
-    selected_vehicles = vehicles.select {|vehicle| vehicle.stops_away(stop) > 0}
-    sorted_vehicles = selected_vehicles.sort_by do |vehicle|
-      vehicle.stops_away(stop)
-    end
-    strings = sorted_vehicles.map{|vehicle| vehicle.stops_away_string(stop)}
-    if strings.empty?
+    statuses = status_updates
+    if statuses.empty?
       "found no updates on buses stopping at #{self.posted_text}"
-    elsif strings.count == 1
-      strings.first
+    elsif statuses.count == 1
+      statuses.first
     else
-      strings[0..1].join(", ")
+      statuses[0..1].join(", ")
     end
+  end
+
+  def status_updates
+    vps.response.empty? ? ["empty API response"] : stops_away_strings
+  end
+
+  def vps
+    @vps = VehiclePositionsService.new(stop)
+  end
+
+  def stops_away_strings
+    vps.sorted_vehicles.map{|vehicle| vehicle.stops_away_string(stop)}
   end
 
 end
